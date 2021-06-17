23ce22dda6ebc45f023fcecdb1de73f1c2402946
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-06-17 14:03:32 -0700

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
 /**
- * main - main block
- * Return: 1
+ * main - print to standard error
+ * Description: write function
+ * Return: return 1
  */
+
 int main(void)
 {
-int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	s);
 return (1);
 }
