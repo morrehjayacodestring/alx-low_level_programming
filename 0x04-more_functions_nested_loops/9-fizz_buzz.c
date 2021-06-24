202699690493c9ce609f5b731d51412a95c32667
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-06-24 08:22:18 -0700

    debugging worked

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,33 +1,41 @@
-#include <stdio.h>
-#include "math.h"
 #include "holberton.h"
+#include <stdio.h>
+
 /**
- * main - main block, solve fizz buzz from numbers 1 to 100
- * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
- * Multiples of both 3 and 5 should print FizzBuzz.
- * Return: 0
+ * main - Fizz for multiples of 3 and Buzz for multiples of 5 FizzBuzz for both
+ *
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-int i;
+  int count = 1;
 
-i = 1;
-while (i <= 100)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-rintf("Buzz");
-else
-printf("%d", i);
+  while (count < 101)
+    {
+      if (count % 3 == 0 && count % 5 == 0)
+	{
+	  printf("%s", "FizzBuzz");
+	}
+      else if (count % 3 == 0)
+	{
+	  printf("%s", "Fizz");
+	}
+      else if (count % 5 == 0)
+	{
+	  printf("%s", "Buzz");
+	}
+      else
+	{
+	  printf("%d", count);
+	}
 
-if (i != 100)
-printf(" ");
-i++;
-}
-printf("\n");
-
-return (0);
+      if (count != 100)
+	{
+	  printf(" ");
+	}
+      count++;
+    }
+  printf("\n");
+  return (0);
 }
