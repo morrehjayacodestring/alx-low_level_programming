9ca98519f42f207c0b6d4fc1477c9b5bd2c51681
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-06-24 14:03:50 -0700

    db

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-more_numbers();
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+print_number(-98);
+_putchar('\n');
 return (0);
 }
