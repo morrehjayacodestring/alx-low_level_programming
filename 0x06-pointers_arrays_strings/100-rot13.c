28e42cffd479cde9f8e47e46bd4e4f238f43b467
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-07-01 10:53:37 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,50 @@
+/*
+ * File: 8-rot13.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "holberton.h"
+
 /**
- * *rot13 - encodes a string using rot13.
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
  *
- * @str: string.
- * Return: string.
+ * Return: A pointer to the encoded string.
  */
 char *rot13(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int indx1 = 0, indx2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+	       'G', 'H', 'I', 'J', 'K', 'L',
+	       'M', 'N', 'O', 'P', 'Q', 'R',
+	       'S', 'T', 'U', 'V', 'W', 'X',
+	       'Y', 'Z', 'a', 'b', 'c', 'd',
+	       'e', 'f', 'g', 'h', 'i', 'j',
+	       'k', 'l', 'm', 'n', 'o', 'p',
+	       'q', 'r', 's', 't', 'u', 'v',
+	       'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+	       'T', 'U', 'V', 'W', 'X', 'Y',
+	       'Z', 'A', 'B', 'C', 'D', 'E',
+	       'F', 'G', 'H', 'I', 'J', 'K',
+	       'L', 'M', 'n', 'o', 'p', 'q',
+	       'r', 's', 't', 'u', 'v', 'w',
+	       'x', 'y', 'z', 'a', 'b', 'c',
+	       'd', 'e', 'f', 'g', 'h', 'i',
+	       'j', 'k', 'l', 'm'};
+while (str[indx1])
 {
-if ((*(str + i) >= 'a' && *(str + i) < 'n')
-|| (*(str + i) >= 'A' && *(str + i) < 'N'))
+for (indx2 = 0; indx2 < 52; indx2++)
 {
-*(str + i) += 13;
-}
-else if ((*(str + i) >= 'n' && *(str + i) <= 'z')
-|| (*(str + i) >= 'N' && *(str + i) <= 'Z'))
+if (str[indx1] == alphabet[indx2])
 {
-*(str + i) -= 13;
+str[indx1] = rot13key[indx2];
+break;
 }
 }
+
+indx1++;
+}
+
 return (str);
 }
