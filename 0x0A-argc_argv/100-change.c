213a86f957bb69168165895832282b58433cf387
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-07-12 07:30:33 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,31 +2,26 @@
  * File: 100-change.c
  * Auth: morris mutisya sammy 
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
 /**
  * main - Prints the minimum number of coins to
- *        make change for an amount of money.
+ * make change for an amount of money.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
  * Return: If the number of arguments is not exactly one - 1.
- *         Otherwise - 0.
+ * Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
 int cents, coins = 0;
-
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
 cents = atoi(argv[1]);
-
 while (cents > 0)
 {
 coins++;
@@ -52,8 +47,6 @@ continue;
 }
 cents--;
 }
-
 printf("%d\n", coins);
-
 return (0);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -4,5 +4,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
 int main(int argc, char *argv[]);
-  
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 #endif
