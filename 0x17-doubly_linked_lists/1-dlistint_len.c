d54df55e301765b1870caa4c4d25667f053e7750
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-09-17 02:58:28 -0700

    betty fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,10 +7,9 @@
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
-{
-  size_t i;
-
-  for (i = 0; h != NULL; i++)
-    h = h->next;
-  return (i);
+(
+size_t i;
+for (i = 0; h != NULL; i++)
+h = h->next;
+return (i);
 }
